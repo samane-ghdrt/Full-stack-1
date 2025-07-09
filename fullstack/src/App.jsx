dc070461f144fe3fd559752b2f1d37df695d6ef4
samane ghodrati <samane.ghodrati@gmail.com>
2025-07-08 21:40:07 -0900

    add

--- fullstack/src/App.jsx
+++ fullstack/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TodoList from './component/TodoList'
 
 function App() {
  return(<>
+<TodoList/>
+
  </>)
 }
 
